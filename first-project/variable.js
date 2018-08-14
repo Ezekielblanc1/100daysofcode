a041146871116cc747d912cf4e058194ec7cce3c
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-08-14 09:30:58 +0100

    Code on using variables

---
+++ first-project/variable.js
@@ -0,0 +1,26 @@
+let firstName = "Ezekiel";
+let lastName = "blanc";
+let fullName = firstName + "" + lastName;
+// console.log(fullName);
+
+//First challenge 
+let city = "Portharcourt";
+let country = "Nigeria";
+let location = city + ", "+ country;
+// console.log(location)
+
+
+//Temp converter
+
+let fahrenheit = 32;
+let celsuis = (fahrenheit -32) * 5/9;
+let kelvin = (fahrenheit +459.67) * 5/9;
+console.log(celsuis);
+console.log(kelvin)
+
+//Score calculator
+
+let score = 100;
+let totalScore = 35;
+let finalScore = totalScore/score
+console.log(finalScore);
