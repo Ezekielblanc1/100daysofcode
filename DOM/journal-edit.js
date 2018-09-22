a995acc04c4ada7f34d9587d7b1491ea90b0ae07
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-09-22 20:35:43 +0100

    Added the event listner for the input,textarea and button

--- DOM/journal-edit.js
+++ DOM/journal-edit.js
@@ -1,3 +1,7 @@
+const nameElement = document.querySelector('#name-title')
+const producerElement = document.querySelector('#name-body')
+const removeElement = document.querySelector('#remove-note')
+
 const movieId = location.hash.substring(1)
 const movies = getSavedJournal()
 const movie = movies.find(function(journal){
@@ -7,3 +11,21 @@ const movie = movies.find(function(journal){
 if(movie === undefined){
   location.assign('index.html')
 }
+nameElement.value = movie.name
+producerElement.value = movie.producer
+
+nameElement.addEventListener('input', function(e){
+  movie.name = e.target.value
+  saveMovie(movies)
+})
+
+producerElement.addEventListener('input', function(e){
+  movie.producer = e.target.value 
+  saveMovie(movies)
+})
+
+removeElement.addEventListener('click', function(){
+  removeJournal(movie.id)
+  saveMovie(movies)
+  location.assign('index.html')
+})
