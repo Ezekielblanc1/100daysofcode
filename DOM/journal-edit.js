b17a60ab42ee2e3dfd868ed986fc3b245ad65fe2
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-09-24 23:49:54 +0100

    Added the timestamp function calls to the name and producer input fields

--- DOM/journal-edit.js
+++ DOM/journal-edit.js
@@ -1,6 +1,7 @@
 const nameElement = document.querySelector('#name-title')
 const producerElement = document.querySelector('#name-body')
 const removeElement = document.querySelector('#remove-note')
+const dateElement = document.querySelector('last-edited')
 
 const movieId = location.hash.substring(1)
 const movies = getSavedJournal()
@@ -13,14 +14,18 @@ if(movie === undefined){
 }
 nameElement.value = movie.name
 producerElement.value = movie.producer
-
+dateElement.textContent = generateTimeStamp(movie.updatedAt)
 nameElement.addEventListener('input', function(e){
   movie.name = e.target.value
+  movie.updatedAt = moment().valueof()
+  dateElement.textContent = generateTimeStamp(movie.updatedAt)
   saveMovie(movies)
 })
 
 producerElement.addEventListener('input', function(e){
   movie.producer = e.target.value 
+  movie.updatedAt = moment().valueof()
+  dateElement.textContent = generateTimeStamp(movie.updatedAt)
   saveMovie(movies)
 })
 
@@ -29,4 +34,21 @@ removeElement.addEventListener('click', function(){
   saveMovie(movies)
   location.assign('index.html')
 })
+
+window.addEventListener('storage', function(e){
+  if(e.key === 'notes'){
+    movies = JSON.parse(e.newValue)
+    movie = notes.find(function(note){
+      return movie.id === movieId
+    })
+
+    if(note === undefined){
+      location.assign('index.html')
+    }
+    nameElement.value = movie.name
+    producerElement.value = movie.producer
+    dateElement.textContent = generateTimeStamp(movie.updatedAt)
+  }
+})
+  
 
