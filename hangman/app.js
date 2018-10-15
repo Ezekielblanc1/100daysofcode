afdfc9b704f73fec3c86b2e6e474367ece8e5a96
Ezekiel blanc <ezekielblanc1@gmail.com>
2018-10-15 12:22:28 +0100

    Removed the code handling the request to get puzzle

--- hangman/app.js
+++ hangman/app.js
@@ -15,6 +15,27 @@ window.addEventListener('keypress', (e) => {
 });
 
 
+const puzzle = getPuzzle()
+console.log(puzzle)
+
+
+const countryCode = 'GH'
+
+const countryRequest = new XMLHttpRequest();
+countryRequest.addEventListener('readystatechange', (e)=>{
+  if (e.target.readyState === 4 && e.target.status === 200) {
+    const data = JSON.parse(e.target.responseText)
+    const country = data.find((country) => country.alpha2Code === countryCode)
+    console.log(country.name)
+  } else if (e.target.readyState === 4) {
+    console.log('An error occurred')
+  }
+})
+
+
+countryRequest.open('GET', 'http://restcountries.eu/rest/v2/all')
+countryRequest.send()
+
 
 
 
